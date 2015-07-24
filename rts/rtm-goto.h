aecaa4c6f6fa126987bd5808d5be64c8624df031
Ryan Yates <ryates@cs.rochester.edu>
2015-07-23 20:12:11 -0400

    GCC 5 supports TSX.  Also need to add 'GhcRtsCcOpts=-mrtm' to mk/build.mk

--- rts/rtm-goto.h
+++ rts/rtm-goto.h
@@ -64,7 +64,12 @@ Abort a transaction.
 #define XFAIL(label) label: asm volatile("" ::: "eax")
 #define XFAIL_STATUS(label, status) label: asm volatile("" : "=a" (status))
 
+#if __GNUC__ >= 5
+#include "immintrin.h"
+#else
 #include "rtm.h"
+#endif
+
 #define XTEST() _xtest()
 
 /* Status bits */
